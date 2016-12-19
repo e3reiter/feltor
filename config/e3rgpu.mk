1d0365c4c0648fc47c5fbd10a068590d693b1990
e3r <reiter.eduard@gmail.com>
2016-12-19 16:01:30 +0100

    impurities: default.json, removed default.in; add config for e3r

---
+++ config/e3rgpu.mk
@@ -0,0 +1,11 @@
+ifeq ($(strip $(shell hostname)),e3rgpu)	# uniquely identify system
+CC=g++																		# the host c++ compiler
+MPICC = mpic++
+MPICFLAGS+= -DMPICH_IGNORE_CXX_SEEK
+OPT=-O3                                   # the optimization flag for the host
+OMPFLAG=-fopenmp                          # openmp flag for CC and MPICC
+LIBS=-lnetcdf -lcurl -lhdf5 -lhdf5_hl     # netcdf library for file output
+NVCCARCH=-arch=sm_61                      # nvcc gpu compute capability
+GLFLAGS = -lglfw -lGL
+JSONLIB = -ljsoncpp
+endif
